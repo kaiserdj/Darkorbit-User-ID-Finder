03ea5bd0e8af9d1ab6a0ba865d7d6d076bcfda0b
Kaiserdj <gonferfr@gmail.com>
2020-04-10 16:29:02 +0200

    Añadido form para mostrar datos

--- js/script.js
+++ js/script.js
@@ -1,54 +1,11 @@
-$("#input").on('keypress',function(e) {
-    console.log("a");
-    console.log(e);
-    if(e.which == 13) {
-        let cors = "https://cors-anywhere.herokuapp.com/";
-        let server = "es1";
-        let darkorbit = `https://${server}.darkorbit.com/ajax/pilotprofil.php`;
+/* https://dostats.info/ */
 
-        let username = e.target.value;
-
-        let settings = {
-            "url": `${cors}${darkorbit}`,
-            "type": "POST",
-            "header": '{"X-Requested-With": "XMLHttpRequest"}',
-            "dataType": "json",
-            "data": {
-                "command": "searchProfileFromExternalPPP",
-                "profileUsername": username
-            }
-        };
-
-        $.ajax(settings).done(function (json) {
-            console.log("json answer: ");
-            console.log(json);
-
-            iduser64 = json.url.split("/")[4].split("-")[0];
-
-            let digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-            let iduser10 = 0;
-            for (let i = 0; i < iduser64.length; i++) {
-                let p = digits.indexOf(iduser64[i]);
-                if (p < 0) {
-                    iduser64 = NaN;
-                } else {
-                    iduser10 += p * Math.pow(digits.length, iduser64.length - i - 1);
-                }
-            }
-
-            console.log(`Iduser-64: ${iduser64}`);
-            console.log(`Iduser-10: ${iduser10}`);
-        });
-    }
-});
-
-$(document).keypress(function(event) {
+$(document).keypress(function (event) {
     if (event.key === "Enter" && event.target.id === "input" && event.target.value !== "") {
-        console.log("cargando");
+        console.log("------loading------");
         let cors = "https://cors-anywhere.herokuapp.com/";
         let server = $("#server").val();;
         let darkorbit = `https://${server}.darkorbit.com/ajax/pilotprofil.php`;
-
         let username = event.target.value;
 
         let settings = {
@@ -63,25 +20,42 @@ $(document).keypress(function(event) {
         };
 
         $.ajax(settings).done(function (json) {
+            if ($(".the-form").css("display") === "none") {
+                $(".container").animate({
+                    height: "10%",
+                    "margin-top": "200px"
+                }, 1500);
+                $(".the-form").css("display", "inherit");
+            }
             console.log("json answer: ");
             console.log(json);
+            $("#json").val(JSON.stringify(json));
 
-            iduser64 = json.url.split("/")[4].split("-")[0];
+            if (json.url !== false) {
+                iduser64 = json.url.split("/")[4].split("-")[0];
 
-            let digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-            let iduser10 = 0;
-            for (let i = 0; i < iduser64.length; i++) {
-                let p = digits.indexOf(iduser64[i]);
-                if (p < 0) {
-                    iduser64 = NaN;
-                } else {
-                    iduser10 += p * Math.pow(digits.length, iduser64.length - i - 1);
+                let digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+                let iduser10 = 0;
+                for (let i = 0; i < iduser64.length; i++) {
+                    let p = digits.indexOf(iduser64[i]);
+                    if (p < 0) {
+                        iduser64 = NaN;
+                    } else {
+                        iduser10 += p * Math.pow(digits.length, iduser64.length - i - 1);
+                    }
                 }
+                console.log(`User: ${event.target.value}`);
+                $("#user").val(event.target.value);
+                console.log(`Iduser-64: ${iduser64}`);
+                $("#64").val(iduser64);
+                console.log(`Iduser-10: ${iduser10}`);
+                $("#10").val(iduser10);
+            } else {
+                console.log(`User: ${json.status}`);
+                $("#user").val(json.status);
+                $("#64").val("");
+                $("#10").val("");
             }
-
-            console.log(`Iduser-64: ${iduser64}`);
-            console.log(`Iduser-10: ${iduser10}`);
         });
     }
 });
-
